0db993b02201512e8932338925ca122270c858be
jay <yongje812@gmail.com>
2024-03-23 10:43:20 +0900

    feat: AssetIcon 컴포넌트 추가

---
+++ lib/theme/component/icon/asset_icon.dart
@@ -0,0 +1,29 @@
+import 'package:egomoya/src/service/theme_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AssetIcon extends StatelessWidget {
+  const AssetIcon(
+    this.path, {
+    super.key,
+    this.color,
+    this.size,
+  });
+
+  final String path;
+  final Color? color;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      path,
+      width: size,
+      height: size,
+      colorFilter: ColorFilter.mode(
+        color ?? context.color.text,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
---
+++ lib/theme/component/icon/asset_icon_type.dart
@@ -0,0 +1,53 @@
+enum AssetIconType {
+  arrowLeft,
+  basket,
+  check,
+  chevronRight,
+  close,
+  language,
+  minus,
+  moon,
+  option,
+  plus,
+  search,
+  star,
+  sunny,
+  uncheck,
+}
+
+extension AssetIconTypeExt on AssetIconType {
+  String get _title {
+    switch (this) {
+      case AssetIconType.arrowLeft:
+        return 'arrow-left';
+      case AssetIconType.basket:
+        return 'basket';
+      case AssetIconType.check:
+        return 'check';
+      case AssetIconType.chevronRight:
+        return 'chevron-right';
+      case AssetIconType.close:
+        return 'close';
+      case AssetIconType.language:
+        return 'language';
+      case AssetIconType.minus:
+        return 'minus';
+      case AssetIconType.moon:
+        return 'moon';
+      case AssetIconType.option:
+        return 'option';
+      case AssetIconType.plus:
+        return 'plus';
+      case AssetIconType.search:
+        return 'search';
+      case AssetIconType.star:
+        return 'star';
+      case AssetIconType.sunny:
+        return 'sunny';
+      case AssetIconType.uncheck:
+        return 'uncheck';
+    }
+  }
+
+  String get path => 'assets/icons/$_title.svg';
+}
