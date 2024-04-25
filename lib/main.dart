fa25012af0bd4863ed795ed3ccbb1dddc5df4954
jay <yongje812@gmail.com>
2024-04-26 01:44:38 +0900

    feat: firebase 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:egomoya/src/service/theme_service.dart';
 import 'package:egomoya/src/view/home/home_view.dart';
 import 'package:egomoya/util/route_path.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final prefs = await SharedPreferences.getInstance();
   runApp(
     MultiProvider(providers: [
